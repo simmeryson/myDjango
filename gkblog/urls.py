684380ad661271c1df72542b7058baa4fc8cb217
guok <guok@haierubic.com>
2017-01-14 13:42:46 +0800

    static files

--- gkblog/urls.py
+++ gkblog/urls.py
@@ -5,4 +5,5 @@ from gkblog import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^home/', views.home),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail')
 ]
--- gkblog/views.py
+++ gkblog/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -12,3 +13,11 @@ def index(requese):
 def home(request):
     post_list = Article.objects.all()
     return render(request, 'gkblog/home.html', {'post_list': post_list})
+
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except:
+        raise Http404
+    return render(request, 'gkblog/post.html', {'post': post})
