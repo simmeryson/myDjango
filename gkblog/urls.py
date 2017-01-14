a9714ecc7a52ccc8a0d1656d7bf3c05754e1083e
guok <guok@haierubic.com>
2017-01-14 14:34:22 +0800

    url tag aboutme archives

--- gkblog/urls.py
+++ gkblog/urls.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import url
 
 from gkblog import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # template中的url标签根据先name来匹配.
+    # r''就是url地址的显示.如果url标签和name匹配上,url地址和实际传入参数可以不一样
     url(r'^home/', views.home),
-    url(r'^(?P<id>\d+)/$', views.detail, name='detail')
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
+    url(r'^archives', views.archives, name='archives'),
+    url(r'^aboutme', views.about_me, name='aboutme'),
+    url(r'^tag(?P<tag>\w+)/$', views.search_tag, name='search_tag'),
 ]
--- gkblog/views.py
+++ gkblog/views.py
@@ -21,3 +21,23 @@ def detail(request, id):
     except:
         raise Http404
     return render(request, 'gkblog/post.html', {'post': post})
+
+
+def archives(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'gkblog/archives.html', {'post_list': post_list, 'error': False})
+
+
+def about_me(request):
+    return render(request, 'gkblog/aboutme.html')
+
+
+def search_tag(request, tag):
+    try:
+        post_list = Article.objects.filter(category__iexact=tag)
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'gkblog/tag.html', {'post_list': post_list})
