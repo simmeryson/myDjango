6a8d12940d3d4c70d892d0f1462d7e79dc54eec4
guok <simmeryson@163.com>
2017-04-08 15:06:20 +0800

    质押式回购日报,货币市场基准利率市场日报,利率互换日报,买断式回购日报,现券买卖日报

---
+++ webscraping/chinamoney/InterestRateSwapDaily.py
@@ -0,0 +1,133 @@
+# -*- coding:utf-8 -*-
+# 保存 利率互换日报.按日查询
+import re
+
+import MySQLdb
+import bs4
+import time
+
+from webscraping.chinamoney.scraping import Scraping
+from webscraping.db_manage import DbManager
+
+DB_NAME = 'chinamoney'
+TABLE_NAME = 'InterestRateSwapDaily'
+
+url = "http://www.chinamoney.com.cn/fe-c/InterestRateSwapDailySearchAction.do"
+
+post_para = {'searchDate': '', 'langCode': 'ZH'
+             }
+
+singleDay = [('2017-03-07', '2017-03-07')]
+
+date_list = [('2017-03-08', '2017-04-08')]
+
+
+# 创建表TABLE_NAME
+def create_table_sql():
+    sql = "CREATE TABLE IF NOT EXISTS %s" \
+          "(id int(11) NOT NULL AUTO_INCREMENT," \
+          "date DATE NOT NULL , " \
+          "`参考利率` VARCHAR (30) NOT NULL ," \
+          "`期限` VARCHAR (20)," \
+          "`固定利率` FLOAT ," \
+          "PRIMARY KEY(id)" \
+          ")" \
+          "ENGINE=InnoDB " \
+          "AUTO_INCREMENT=1" \
+          % TABLE_NAME
+
+    return sql
+
+
+def insert_db_value(row):
+    return (row[0], row[1], row[2] or None, row[3] or None
+            )
+
+
+def insert_db_sql_values():
+    # 引号坑死人.字段内不能加特殊符号 比如%
+    sql = "insert into InterestRateSwapDaily (date,`参考利率`,`期限`,`固定利率`)" \
+          " VALUES (%s, %s, %s, %s)"
+    return sql
+
+
+# 解析曲线数据
+def parse_html(html, save_row, insert_into):
+    date = html.find("img", attrs={"class": "icon-calendar"}). \
+        next.string.encode('utf-8').replace("\xc2\xa0", "").strip()
+    try:
+        tds = html.find_all('td', attrs={'class': 'dreport-row2-1 tdline'})
+        for td in tds:
+            field = td.string.encode('utf-8').replace("\xc2\xa0", "").strip()
+            trs = td.find_next_sibling('td').find_all('tr')
+            parse_trs(date, insert_into, save_row, trs, field)
+
+        trs = html.find(string=re.compile('Shibor_3M')).parent.find_next_sibling('td').find_all('tr')
+        parse_trs(date, insert_into, save_row, trs, 'Shibor_3M')
+
+    except MySQLdb.Error, e:
+        print "Mysql Error %d: %s on Table %s" % (e.args[0], e.args[1], TABLE_NAME)
+    except:
+        print "%s no data!" % TABLE_NAME
+    finally:
+        print "%s 抓取完成" % TABLE_NAME
+
+
+# 组合每行的数据并插入
+def parse_trs(date, insert_into, save_row, trs, field_name):
+    for tr in trs:
+        row = [date, field_name]
+        for td in tr.find_all('td'):
+            if td.string:
+                str_ = td.string.encode('utf-8').replace("\xc2\xa0", "").strip()
+                row.append(str_ if str_ != '---' else None)
+        if len(row) == 4:
+            insert_into(save_row(), insert_db_value(row))
+        else:
+            print "wrong row size: " + " ".join(row)
+
+
+def do_scraping(dates_list):
+    scraper = Scraping(url)
+    db_name = DbManager()
+    db_name.create_db(DB_NAME)
+    # db_name.drop_table(TABLE_NAME)
+    db_name.create_table(create_table_sql())
+
+    for dates in dates_list:
+        day_list = scraper.gen_day_list(dates[0], dates[1])
+        scrap_data(day_list, db_name, scraper)
+
+    db_name.close_db()
+
+
+# 抓取曲线数据
+def scrap_data(dates_list, db, scraper):
+    for date in dates_list:
+        para = {'searchDate': date}
+        scraper.make_post_para(para)
+        html = scraper.send_request_post()
+        parse_html(html, insert_db_sql_values, db.insert_db_values)
+        time.sleep(2)
+
+
+# do_scraping(date_list)
+
+
+# 抓取当天数据
+def scraping_today():
+    scraper = Scraping(url)
+    db_name = DbManager()
+    db_name.create_db(DB_NAME)
+    # db_name.drop_table(TABLE_NAME)
+    db_name.create_table(create_table_sql())
+
+    today = str(scraper.get_today_date())
+    day_list = [today]
+
+    scrap_data(day_list, db_name, scraper)
+
+    db_name.close_db()
+
+
+scraping_today()
--- webscraping/chinamoney/MoneyBenchmarkMarketDaily.py
+++ webscraping/chinamoney/MoneyBenchmarkMarketDaily.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# 保存 同业拆借日报.按日查询
+# 保存 货币市场基准利率市场日报.按日查询
 import re
 
 import MySQLdb
@@ -10,11 +10,11 @@ from webscraping.chinamoney.scraping import Scraping
 from webscraping.db_manage import DbManager
 
 DB_NAME = 'chinamoney'
-TABLE_NAME = 'CreditLendDaily'
+TABLE_NAME = 'MoneyBenchmarkMarketDaily'
 
-url = "http://www.chinamoney.com.cn/fe-c/CreditLendDailySearchAction.do"
+url = "http://www.chinamoney.com.cn/fe-c/moneyBenchmarkMarketDailySearchAction.do"
 
-post_para = {'searchDate': '', 'remarkBondType': '261'
+post_para = {'searchDate': ''
              }
 
 singleDay = [('2017-03-07', '2017-03-07')]
@@ -24,44 +24,46 @@ date_list = [('2017-03-08', '2017-04-08')]
 
 # 创建表TABLE_NAME
 def create_table_sql():
-    sql = "CREATE TABLE IF NOT EXISTS CreditLendDaily " \
+    sql = "CREATE TABLE IF NOT EXISTS %s" \
           "(id int(11) NOT NULL AUTO_INCREMENT," \
           "date DATE NOT NULL , " \
-          "`品种` VARCHAR (20) NOT NULL ," \
-          "`开盘利率` FLOAT ," \
-          "`收盘利率` FLOAT ," \
+          "`品种` VARCHAR (30) NOT NULL ," \
+          "`最高利率` FLOAT ," \
+          "`最低利率` FLOAT ," \
           "`加权利率` FLOAT ," \
           "`升降(基点)` FLOAT ," \
-          "`平均拆借期限(天)` FLOAT ," \
-          "`成交笔数(笔)` INT (10)," \
-          "`成交金额(亿元)` FLOAT ," \
+          "`成交金额(亿元)` FLOAT," \
           "`增减(亿元)` FLOAT ," \
           "PRIMARY KEY(id)" \
           ")" \
           "ENGINE=InnoDB " \
-          "AUTO_INCREMENT=1"
+          "AUTO_INCREMENT=1" \
+          % TABLE_NAME
+
     return sql
 
 
 def insert_db_value(row):
     return (row[0], row[1], row[2] or None, row[3] or None, row[4] or None, row[5] or None,
-            row[6] or None, row[7] or None, row[8] or None, row[9] or None
+            row[6] or None, row[7] or None
             )
 
 
 def insert_db_sql_values():
     # 引号坑死人.字段内不能加特殊符号 比如%
-    sql = "insert into CreditLendDaily (date,`品种`,`开盘利率`,`收盘利率`,`加权利率`, `升降(基点)`," \
-          "`平均拆借期限(天)`,`成交笔数(笔)`,`成交金额(亿元)`,`增减(亿元)`)" \
-          " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    sql = "insert into MoneyBenchmarkMarketDaily (date,`品种`,`最高利率`,`最低利率`,`加权利率`, `升降(基点)`," \
+          "`成交金额(亿元)`,`增减(亿元)`)" \
+          " VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
     return sql
 
 
 # 解析曲线数据
 def parse_html(html, save_row, insert_into):
-    date = html.find("img", attrs={"class": "icon-calendar"}).next.string.encode('utf-8').replace("\xc2\xa0", "").strip()
-    trs = html.find("td", attrs={"class": "dreport-title-1", "height": "28", "width": "8%"}).parent.find_next_siblings(
-        'tr')
+    date = html.find("img", attrs={"class": "icon-calendar"}). \
+        next.string.encode('utf-8').replace("\xc2\xa0", "").strip()
+    trs = html.find("td", attrs={"class": "mbr-title", "height": "28", "colspan": "7",
+                                 "align": "left"}) \
+        .parent.find_next_sibling('tr').find_next_siblings('tr')
     try:
         for tr in trs:
             row = [date]
@@ -69,12 +71,12 @@ def parse_html(html, save_row, insert_into):
                 if td.string:
                     str_ = td.string.encode('utf-8').replace("\xc2\xa0", "").strip()
                     row.append(str_ if str_ != '---' else None)
-            if len(row) == 10:
+            if len(row) == 8:
                 insert_into(save_row(), insert_db_value(row))
             else:
                 print "wrong row size: " + " ".join(row)
     except MySQLdb.Error, e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+        print "Mysql Error %d: %s on Table %s" % (e.args[0], e.args[1], TABLE_NAME)
     except:
         print "%s no data!" % TABLE_NAME
     finally:
@@ -85,6 +87,8 @@ def do_scraping(dates_list):
     scraper = Scraping(url)
     db_name = DbManager()
     db_name.create_db(DB_NAME)
+    # db_name.drop_table(TABLE_NAME)
+    db_name.create_table(create_table_sql())
 
     for dates in dates_list:
         day_list = scraper.gen_day_list(dates[0], dates[1])
@@ -95,7 +99,6 @@ def do_scraping(dates_list):
 
 # 抓取曲线数据
 def scrap_data(dates_list, db, scraper):
-
     for date in dates_list:
         para = {'searchDate': date}
         scraper.make_post_para(para)
@@ -104,7 +107,8 @@ def scrap_data(dates_list, db, scraper):
         time.sleep(2)
 
 
-# do_scraping(date_list)
+do_scraping(date_list)
+
 
 # 抓取当天数据
 def scraping_today():
@@ -121,5 +125,4 @@ def scraping_today():
 
     db_name.close_db()
 
-
-scraping_today()
+# scraping_today()
