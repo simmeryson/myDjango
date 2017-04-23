c9a6819ec70344aa841d61e1338c955df8231e49
guok <simmeryson@163.com>
2017-04-24 01:40:45 +0800

    挂牌房源

--- webscraping/realestate/fangcom/CourtSellBoard.py
+++ webscraping/realestate/fangcom/CourtSellBoard.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# fang.com 小区二手成交数据, 月查询.
+# fang.com 小区数据, 日查询.
 import re
 import sys
 
@@ -24,8 +24,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 # 住宅 小区
-trade_url = 'chengjiao/-p11-t11/'
-rent_url = 'chengjiao/-p11-t12/'
+trade_url = 'chushou/list/-h332-j340/'
 
 city_list = [('xa', 'XianHousePrice'),
              # ('bj', 'BeijingHousePrice'), ('sh', 'ShanghaiHousePrice'),
@@ -33,10 +32,16 @@ city_list = [('xa', 'XianHousePrice'),
              # ('hz', 'HangzhouHousePrice'),
              ]
 
-table_name = 'fangcomCourtTradeRecord'
+table_name = 'fangcomCourtSellingInfo'
 
-num_re = re.compile(ur'^-?\d+\.?\d*')
-exclude_re = re.compile(ur'-')
+num_re = re.compile(ur'\d*\b')
+date_rx = re.compile(ur'\d{4}/\d+/\d+')
+maner_re = re.compile(ur'满二')
+manwu_re = re.compile(ur'满五')
+tejia_re = re.compile(ur'特价')
+yezhu_re = re.compile(ur'业主发布')
+
+re_list = [manwu_re, maner_re, tejia_re, yezhu_re]
 
 
 #
@@ -46,20 +51,27 @@ def drop_tables(db):
 
 # 总数据
 # creprice.cn的房价数据
-def create_fangcomCourtTradeRecord_table_sql():
-    sql = "CREATE TABLE IF NOT EXISTS fangcomCourtTradeRecord" \
+def create_fangcomCourtSellingInfo_table_sql():
+    sql = "CREATE TABLE IF NOT EXISTS fangcomCourtSellingInfo" \
           "(id int(11) NOT NULL AUTO_INCREMENT," \
+          "HouseNum INT (10), " \
+          "PublishDate DATE NOT NULL , " \
+          "SellingPrice INT , " \
           "RoomNum VARCHAR (20), " \
+          "Area INT (10) NOT NULL , " \
           "FloorInfo VARCHAR (20), " \
           "Direction VARCHAR (10), " \
-          "Area FLOAT , " \
-          "TradeDate DATE , " \
-          "TradePrice INT , " \
-          "PricePerMeter INT , " \
-          "nameId INT NOT NULL , " \
+          "Decor VARCHAR (10), " \
+          "HouseHistory VARCHAR (10), " \
+          "Telephone VARCHAR (30), " \
+          "`满五唯一` TINYINT (1), " \
+          "`满二` TINYINT (1), " \
+          "`特价房` TINYINT (1)," \
+          "`业主发布` TINYINT (1)," \
+          "nameId INT NOT NULL ," \
           "FOREIGN Key(nameId) REFERENCES  fangcomSecondHandCourtName(id) on delete cascade on update cascade, " \
           "PRIMARY KEY(id)," \
-          "UNIQUE KEY (Area, TradeDate, TradePrice, PricePerMeter)" \
+          "UNIQUE KEY (Area, RoomNum, FloorInfo, nameId)" \
           ")" \
           "ENGINE=InnoDB " \
           "AUTO_INCREMENT=1 " \
@@ -67,53 +79,63 @@ def create_fangcomCourtTradeRecord_table_sql():
     return sql
 
 
-def insert_fangcomCourtTradeRecord_sql_values():
+def insert_fangcomCourtSellingInfo_sql_values():
     # 引号坑死人.字段内不能加特殊符号 比如%
-    sql = "insert into fangcomCourtTradeRecord " \
-          "(RoomNum , " \
-          "FloorInfo, " \
-          "Direction , " \
+    sql = "replace into fangcomCourtSellingInfo " \
+          "(HouseNum , " \
+          "PublishDate , " \
+          "SellingPrice , " \
+          "RoomNum , " \
           "Area , " \
-          "TradeDate  , " \
-          "TradePrice , " \
-          "PricePerMeter , " \
-          "nameId" \
+          "FloorInfo , " \
+          "Direction , " \
+          "Decor , " \
+          "HouseHistory , " \
+          "Telephone , " \
+          "`满五唯一` TINYINT (1), " \
+          "`满二` TINYINT (1), " \
+          "`特价房` TINYINT (1)," \
+          "`业主发布` TINYINT (1)," \
+          "nameId ," \
           ") " \
-          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s)"
     return sql
 
 
-def insert_fangcomCourtTradeRecord_value(row):
+def insert_fangcomCourtSellingInfo_value(row):
     return (row[0], row[1] or None, row[2] or None, row[3] or None,
             row[4] or None, row[5] or None, row[6] or None,
-            row[7] or None
+            row[7] or None, row[8] or None, row[9] or None,
+            row[10] or None, row[11] or None, row[12] or None,
+            row[13] or None, row[14]
             )
 
 
-def parse_trade_html(html, court_id, db):
+def parse_trade_html(html, court_id, db, scraper):
     try:
-        record_list = html.find('div', class_='dealSent sentwrap').find_all('tr')
-        for line in record_list[1:]:
+        for p in html.find_all('p', class_='fangTitle'):
+            # 请求房源详细信息
+            url = p.a['href']
+            scraper.make_header_para({'Referer': scraper.url})
+            scraper.url = url
+            html_detail = scraper.send_request_get()
             row = []
-            roomInfo = line.find('td', class_='firsttd')
-            number = roomInfo.div.find('a', attrs={'target': '_blank'})
-            row.append(number.string.encode('utf-8').strip())
-            for floor in number.parent.parent.find_next_siblings('p'):
-                row.append(floor.string.encode('utf-8').strip() if floor and floor.string else None)
-            for trading in roomInfo.find_next_siblings('td')[:-1]:
-                string = trading.b.string.encode('utf-8').strip() if trading.b \
-                    else trading.string.encode('utf-8').strip()
-                ex = exclude_re.findall(string, 0)
-                if len(ex) > 0:
-                    row.append(string)
-                else:
-                    b = num_re.findall(string, 0)
-                    row.append(b[0] if len(b) > 0 else '0')
+            spans = html_detail.find('p', class_='gray9 titleAdd').find_all('span')
+            house_num = num_re.findall(spans[-2].string.encode('utf-8').strip())
+            row.append(house_num[0] if len(house_num) > 0 else None)
+            publish = date_rx.findall(spans[-1].string.encode('utf-8').strip())
+            row.append(publish[0] if len(publish) > 0 else None)
+
+            # 标签
+            for span in spans[:-2]:
+                for rx in re_list:
+                    if len(rx.findall(span.string.encode('utf-8').strip())) > 0:
+                        row.append(1)
 
             row.append(court_id)
             if len(row) == 8:
-                db.insert_db_values(insert_fangcomCourtTradeRecord_sql_values(),
-                                    insert_fangcomCourtTradeRecord_value(row))
+                db.insert_db_values(insert_fangcomCourtSellingInfo_sql_values(),
+                                    insert_fangcomCourtSellingInfo_value(row))
             else:
                 print "wrong row size: " + " ".join(row)
     except MySQLdb.Error, e:
@@ -126,7 +148,7 @@ def parse_trade_html(html, court_id, db):
 
 def scrap_detail(db):
     # drop_tables(db)
-    db.create_table(create_fangcomCourtTradeRecord_table_sql())
+    db.create_table(create_fangcomCourtSellingInfo_table_sql())
     court_list = db.query('select id, name, DetailUrl from fangcomSecondHandCourtName ')
     for (court_id, court_name, court_url) in court_list:
         print '小区:' + court_name + "  开始获取详情"
@@ -144,7 +166,7 @@ def send_request(court_id, court_url, db, url, scraper):
     scraper.url = url
     scraper.make_header_para({'Referer': court_url})
     html = scraper.send_request_get()
-    parse_trade_html(html, court_id, db)
+    parse_trade_html(html, court_id, db, scraper)
     time.sleep(0.5)
     next_page = html.find('a', id='ctl00_hlk_next')
     if next_page:
